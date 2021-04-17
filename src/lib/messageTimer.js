9fe6ec388f6f063d54b589576ad44d045b207df6
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-04-17 18:24:37 +0100

    refactored

--- src/lib/messageTimer.js
+++ src/lib/messageTimer.js
@@ -14,6 +14,7 @@ const getDifferenceInHours = (date2, date1) => {
 const energyMessage = () => {
   const currentHour = new Date().getHours();
   console.log(`Current hour: ${currentHour}`);
+
   if (currentHour >= 7 && currentHour <= 20) {
     axios.get("https://api.reactive.energy/energy-mix").then((res) => {
       const energyMix = res.data;
@@ -23,60 +24,59 @@ const energyMessage = () => {
         energyMix.biomass.proportion +
         energyMix.hydro.proportion +
         energyMix.nuclear.proportion;
+
       if (currentGreenEnergyProportion > 0.3) {
         const currentTime = new Date();
-
-        // put timePrevMessageSent logic here
-
         Subscriber.findAll({
           where: {
             subscribe: true,
           },
-        })
-          .then((subscribers) => {
-            timePreviousMessageSent = subscribers[0].messageSent;
-            console.log(`Previous message sent at: ${timePreviousMessageSent}`);
-            timeElapsedSincePreviousMessage = getDifferenceInHours(
-              currentTime,
-              timePreviousMessageSent
-            );
-            console.log(
-              `Time elapsed since previous message: ${timeElapsedSincePreviousMessage}`
-            );
-          })
-          .then(() => {
-            if (
-              timePreviousMessageSent === null ||
-              timeElapsedSincePreviousMessage > 10
-            ) {
-              Subscriber.findAll({
-                where: {
-                  subscribe: true,
-                },
+        }).then((subscribers) => {
+          timePreviousMessageSent = subscribers[0].messageSent;
+
+          console.log(`Previous message sent at: ${timePreviousMessageSent}`);
+
+          timeElapsedSincePreviousMessage = getDifferenceInHours(
+            currentTime,
+            timePreviousMessageSent
+          );
+
+          console.log(
+            `Time elapsed since previous message: ${timeElapsedSincePreviousMessage}`
+          );
+
+          if (
+            timePreviousMessageSent === null ||
+            timeElapsedSincePreviousMessage > 10
+          ) {
+            Subscriber.findAll({
+              where: {
+                subscribe: true,
+              },
+            })
+              .then((subscribers) => {
+                messageSender.sendMessageToSubscribers(
+                  subscribers,
+                  `From test app: Current low carbon energy proportion is ${Math.floor(
+                    currentGreenEnergyProportion * 100
+                  )}%. You are an eco-champion!`
+                );
               })
-                .then((subscribers) => {
-                  messageSender.sendMessageToSubscribers(
-                    subscribers,
-                    `From test app: Current low carbon energy proportion is ${Math.floor(
-                      currentGreenEnergyProportion * 100
-                    )}%. You are an eco-champion!`
-                  );
-                })
-                .then(() => {
-                  res
-                    .status(200)
-                    .send("Success! Your message(s) have been sent.");
-                })
-                .catch((err) => {
-                  console.log(`Err ${err.message}`);
-                  res.status(500).send(err.message);
-                });
-            } else {
-              return console.log(
-                "Status: It has not been 10 hours since the previous message. No message sent."
-              );
-            }
-          });
+              .then(() => {
+                res
+                  .status(200)
+                  .send("Success! Your message(s) have been sent.");
+              })
+              .catch((err) => {
+                console.log(`Err ${err.message}`);
+                res.status(500).send(err.message);
+              });
+          } else {
+            return console.log(
+              "Status: It has not been 10 hours since the previous message. No message sent."
+            );
+          }
+        });
       } else {
         return console.log(
           `Status: Current low carbon energy proportion is only ${Math.floor(
